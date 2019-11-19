e7fdc364e3d5d2d60cd990f866533170266149bc
AdamGibbs <gibbsa3@cardiff.ac.uk>
2019-11-19 12:07:17 +0000

    Update OpenMapJS to work with set locations and markers

--- static/js/OpenMapData.js
+++ static/js/OpenMapData.js
@@ -1,12 +1,65 @@
+map = new OpenLayers.Map("map");
+map.addLayer(new OpenLayers.Layer.OSM());
+
+epsg4326 =  new OpenLayers.Projection("EPSG:4326"); //WGS 1984 projection
+projectTo = map.getProjectionObject(); //The map projection (Spherical Mercator)
+
+var lonLat = new OpenLayers.LonLat( -0.1279688 ,51.5077286 ).transform(epsg4326, projectTo);
+        
+
+var zoom=14;
+map.setCenter (lonLat, zoom);
+
+var vectorLayer = new OpenLayers.Layer.Vector("Overlay");
+
+// Define markers as "features" of the vector layer:
+var feature = new OpenLayers.Feature.Vector(
+        new OpenLayers.Geometry.Point( -0.1279688, 51.5077286 ).transform(epsg4326, projectTo),
+        {description:'This is the value of<br>the description attribute'} ,
+        {externalGraphic: 'img/marker.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
+    );    
+vectorLayer.addFeatures(feature);
+
+var feature = new OpenLayers.Feature.Vector(
+        new OpenLayers.Geometry.Point( -0.1244324, 51.5006728  ).transform(epsg4326, projectTo),
+        {description:'Big Ben'} ,
+        {externalGraphic: 'img/marker.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
+    );    
+vectorLayer.addFeatures(feature);
+
+var feature = new OpenLayers.Feature.Vector(
+        new OpenLayers.Geometry.Point( -0.119623, 51.503308  ).transform(epsg4326, projectTo),
+        {description:'London Eye'} ,
+        {externalGraphic: 'img/marker.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
+    );    
+vectorLayer.addFeatures(feature);
 
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+map.addLayer(vectorLayer);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-	maxZoom: 18,
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	id: 'mapbox.streets'
-}).addTo(mymap);
 
+//Add a selector control to the vectorLayer with popup functions
+var controls = {
+    selector: new OpenLayers.Control.SelectFeature(vectorLayer, { onSelect: createPopup, onUnselect: destroyPopup })
+};
+
+function createPopup(feature) {
+    feature.popup = new OpenLayers.Popup.FramedCloud("pop",
+        feature.geometry.getBounds().getCenterLonLat(),
+        null,
+        '<div class="markerContent">'+feature.attributes.description+'</div>',
+        null,
+        true,
+        function() { controls['selector'].unselectAll(); }
+    );
+    //feature.popup.closeOnMove = true;
+    map.addPopup(feature.popup);
+}
+
+function destroyPopup(feature) {
+    feature.popup.destroy();
+    feature.popup = null;
+}
+
+map.addControl(controls['selector']);
+controls['selector'].activate();
