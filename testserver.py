fb29113b69238d150a65f62213f24371219e0dc0
AdamGibbs <gibbsa3@cardiff.ac.uk>
2019-11-21 12:00:12 +0000

    Useage of bootstrap and templating for webpage, creating a basic homepage

--- testserver.py
+++ testserver.py
@@ -2,10 +2,15 @@ from flask import Flask, redirect, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/Where", methods = ['GET'])
+@app.route("/where", methods = ['GET'])
 def WherePage():
 	if request.method =='GET':
 		return redirect('/static/index.html')
+
+@app.route("/home", methods = ['GET'])
+def HomePage():
+	if request.method =='GET':
+		return render_template('HomePage.html')
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5000, ssl_context='adhoc')
