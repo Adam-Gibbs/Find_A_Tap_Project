65cc1a7116fd2b6a740ec9d4df35aba35aa29fdb
Osama Ilyas <IlyasO@cardiff.ac.uk>
2019-11-22 17:43:16 +0000

    Added a server route and a into paragraph to the comments page

--- server.py
+++ server.py
@@ -97,5 +97,6 @@ def page_not_found(e):
   return render_template('404.html'), 404
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000, ssl_context='adhoc')
+    app.run(debug=True)
+	# app.run(host='0.0.0.0', port=5000, ssl_context='adhoc')
 
