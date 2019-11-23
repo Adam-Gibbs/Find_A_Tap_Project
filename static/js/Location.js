78d1d490be75882622d58e8d3c255ec1fff12b65
Nimrod Shefi <ShefiN@cardiff.ac.uk>
2019-11-23 16:14:08 +0000

    Loading the add tap page to the project. Not html only functionality

--- static/js/Location.js
+++ static/js/Location.js
@@ -1,5 +1,6 @@
 // function getLocation taken from w3school - https://www.w3schools.com/html/html5_geolocation.asp
 function getLocation() {
+  console.log("hello")
   var x = document.getElementById('ERROR');
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
@@ -9,25 +10,23 @@ function getLocation() {
 }
 
 function showPosition(position) {
-  var lngText = document.getElementById('lon');
-  var latText = document.getElementById('lat');
-  var address = getAddress();
-  latText.innerHTML = position.coords.latitude;
-  lngText.innerHTML = position.coords.longitude;
-  sendData(address, lngText, latText);
+  console.log("hello2")
+  // var lngText = document.getElementById('lon');
+  // var latText = document.getElementById('lat');
+  // latText.innerHTML = position.coords.latitude;
+  // lngText.innerHTML = position.coords.longitude;
+  var lngText = position.coords.longitude;
+  var latText = position.coords.latitude;
+  console.log(lngText, latText)
+  sendData(lngText, latText);
 }
 
-function getAddress(){
-    var address = prompt("What is the address of the water tap? ");
-    return address;
-}
-
-function sendData(address, long, lat){
-  var coordinates = lat.innerHTML +","+ long.innerHTML;
-  var params = "address="+address+"&coordinates="+coordinates;
+function sendData(long, lat){
+  var coordinates = lat+","+ long;
+  var params = "coordinates="+coordinates;
   console.log(params);
   var IPPacket = new XMLHttpRequest();
-  IPPacket.open("POST", '/saveCoordinates', true); // true is asynchronous
+  IPPacket.open("POST", '/home/taps/new', true); // true is asynchronous
   IPPacket.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   IPPacket.send(params);
   console.log(params)
