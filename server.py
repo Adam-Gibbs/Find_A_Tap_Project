4d3fd6a6a0aff404a5dc2c8db008da82fd4ae2ad
Nimrod Shefi <ShefiN@cardiff.ac.uk>
2019-11-23 20:49:42 +0000

    removing an unnecessary route

--- server.py
+++ server.py
@@ -14,32 +14,6 @@ app = Flask(__name__)
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 directory = []
 
-@app.route("/saveCoordinates", methods=['POST','GET'])
-def addLocation():
-    if request.method =='GET':
-        return redirect('/static/index.html')
-    elif request.method =='POST':
-        print(type(request.form))
-        params = request.form
-        params = params.to_dict() # This is from flask
-        print("------------------------------------------------------------------------",params)
-        coordinates = params['coordinates']
-        address = params['address']
-        try:
-            conn = sqlite3.connect(DATABASE)
-            cur = conn.cursor()
-            cur.execute("INSERT INTO taps (address, coordinates) VALUES (?,?)",
-            (address, coordinates))
-            conn.commit()
-            executed = True
-        except:
-            conn.rollback()
-            print("An error has occured when accessing the database")
-            executed = False
-        finally:
-            conn.close()
-            return f"Task was executed: {executed}"
-
 @app.route("/Directory", methods=['GET'])
 def returnDir():
     if request.method == 'GET':
