cc7b282238ffed024bfbaf9702dfa114d142c4b7
Nimrod Shefi <ShefiN@cardiff.ac.uk>
2019-11-25 18:15:49 +0000

    starting to work on adding pictures to the database. Also created a check for the coordinates, making sure that there aren't doubles

--- server.py
+++ server.py
@@ -6,12 +6,16 @@ import sqlite3
 # FOR THIS TO WORK YOU NEED TO ON YOUR CMD TO DO THIS: pip install opencage
 from opencage.geocoder import OpenCageGeocode
 from opencage.geocoder import InvalidInputError, RateLimitExceededError, UnknownError
+from werkzeug.utils import secure_filename
 key = 'd0d06fa6997b4770af8c48796657cbf0'
 geocoder = OpenCageGeocode(key)
 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))# this
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/uploads')
 DATABASE = 'databases/main_db.db'
 app = Flask(__name__)
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 directory = []
 
 
@@ -65,24 +69,40 @@ def NewTapPage():
         longitude = params['longitude']
         address = geocoder.reverse_geocode(latitude, longitude, language='en', no_annotations='1')
         address = address[0]['formatted']
-        print(address)
         picture = params['picture']
+        # picture = request.files['picture']
         print(picture)
+        if len(picture) > 0:
+            picture_extension = picture.split(".")[1].lower()
+            print(picture_extension)
+            if picture_extension not in ALLOWED_EXTENSIONS:
+                picture = None
+                # this makes sure that only picture type files are uploaded to the website
+        if picture != None and len(picture) != 0: # This means that the picture exists
+            picture = picture.split("\\")[2]
+            print("------------------------------------------------------------------------------------------------------------------------------",picture)
+            # filename = secure_filename(picture.filename)
+            # filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            # picture.save(filepath)
+            # print("Done")
         try:
             conn = sqlite3.connect(DATABASE)
             cur = conn.cursor()
-            cur.execute("INSERT INTO taps (address, latitude, longitude, picture) VALUES (?,?,?,?)",
-            (address, latitude, longitude, picture))
-            conn.commit()
-            msg = "Task was executed: True"
+            cur.execute("SELECT latitude, longitude FROM taps WHERE latitude=? AND  longitude=?", (latitude, longitude))
+            data = cur.fetchall()
+            ## THIS IF STATEMENT MAKES SURE THAT TAPS THAT ALREADY EXIST IN THE DATABASE CANNOT BE INPUTTEED AGAIN
+            if len(data) == 0:
+                cur.execute("INSERT INTO taps (address, latitude, longitude, picture) VALUES (?,?,?,?)",
+                (address, latitude, longitude, picture))
+                conn.commit()
+                msg = "Task was executed"
+            else:
+                msg = "Tap already exists in the database"
+                #alert(msg)
         except Exception as e:
             print(e)
             conn.rollback()
             msg = f"Task failed because: {e}"
-            if e == "UNIQUE constraint failed: taps.coordinates":
-                msg = ''' var error = document.getElementById('error-message');
-                    error.innerHTML = Tap already exists;
-                '''
         finally:
             conn.close()
             return msg
