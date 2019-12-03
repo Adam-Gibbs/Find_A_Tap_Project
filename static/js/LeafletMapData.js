a7cc175187768a68bcbc647074f16831608d7d98
AdamGibbs <gibbsa3@cardiff.ac.uk>
2019-12-03 11:48:10 +0000

    Add routing to tap list from current pos

--- static/js/LeafletMapData.js
+++ static/js/LeafletMapData.js
@@ -8,16 +8,42 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
     id: 'mapbox.streets'
 }).addTo(mymap);
 
+L.Routing.control({
+    lineOptions: {
+        styles: [{color: 'blue', weight: 7}],
+        router: L.Routing.mapbox('pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+            profile : 'mapbox/walking'
+        })
+    }
+})
+
 function AddMarker(latitude, longitude, popup) {
     L.marker([latitude, longitude]).addTo(mymap)
         .bindPopup(popup).openPopup();
 }
 
-function Route(latitudeFrom, longitudeFrom, latitudeTo, longitudeTo) {
-    L.Routing.control({
-        waypoints: [
-            L.latLng(latitudeFrom, longitudeFrom),
-            L.latLng(latitudeTo, longitudeTo)
-        ]
-        }).addTo(map);
-}
+function RouteFromMe(latitudeTo, longitudeTo) {
+    var x = document.getElementById('ERROR');
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(Route);
+    } else {
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+
+    function Route(position) {
+        longitudeFrom = position.coords.longitude;
+        latitudeFrom = position.coords.latitude;
+        L.Routing.control({
+            lineOptions: {
+                styles: [{color: 'blue', weight: 7}],
+            },
+            router: L.Routing.mapbox('pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                profile : 'mapbox/walking'
+            }),
+            waypoints: [
+                L.latLng(latitudeFrom, longitudeFrom),
+                L.latLng(latitudeTo, longitudeTo)
+            ]
+            }).addTo(mymap);
+    }
+  }
